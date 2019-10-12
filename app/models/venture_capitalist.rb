7a33404b1246e5f2fc6c6a3a7d57c65497fd8b0c
DannyBrito <britodanny1997@gmail.com>
2019-10-12 18:08:05 -0400

    Done.

--- app/models/venture_capitalist.rb
+++ app/models/venture_capitalist.rb
@@ -1,3 +1,45 @@
 class VentureCapitalist
 
+    attr_accessor :name
+    @@all =[]
+
+    def self.all
+        @@all
+    end
+
+    def initialize(name, net_worth)
+        @name = name
+        @net_worth = net_worth
+
+        self.class.all << self
+
+    end
+
+    def total_worth
+        @net_worth
+    end
+
+    def self.tres_commas_club
+        all.select{|investor| investor.total_worth > 1000000000}
+    end
+
+    def offer_contract(startup, investment, type = "A1")
+        FundingRound.new(startup, self, type, investment)
+    end
+
+    def funding_rounds
+        FundingRound.all.select{|investment| investment.venture_capitalist == self }
+    end
+
+    def portfolio
+        funding_rounds.map{|inv| inv.startup}.uniq
+    end
+
+    def biggest_investment
+        funding_rounds.max_by{|inv| inv.investment}
+    end
+
+    def invested(domain)
+        funding_rounds.select{|inv| inv.startup.domain == domain}.map{|inv| inv.investment}.sum
+    end
 end
