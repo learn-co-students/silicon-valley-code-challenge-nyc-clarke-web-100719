28540d9bbd79e83227920e289a0bd9fce6345938
Jimmy Cheung <jimmy.h.cheung@gmail.com>
2019-10-13 23:46:41 -0400

    Done.

--- app/models/funding_round.rb
+++ app/models/funding_round.rb
@@ -1,3 +1,22 @@
 class FundingRound
+    attr_accessor :type
+    attr_reader :startup, :venture_capitalist, :investment
+    
+    @@all = []
 
+    def initialize(startup, venture_capitalist, type, investment)
+        @startup = startup
+        @venture_capitalist = venture_capitalist
+        @type = type
+        @investment = investment
+        self.class.all << self
+    end
+
+    def investment=(investment)
+        @investment = investment.to_f unless investment < 0
+    end
+
+    def self.all
+        @@all
+    end
 end
--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,54 @@
 class Startup
+    attr_accessor :name
+    attr_reader :founder, :domain
 
+    @@all = []
+
+    def initialize(name, founder, domain)
+        @name = name
+        @founder = founder
+        @domain = domain
+        self.class.all << self
+    end
+
+    def pivot(domain, name)
+        @domain = domain
+        self.name = name
+    end
+
+    def sign_contract(venture_capitalist, type, investment)
+        FundingRound.new(self, venture_capitalist, type, investment)
+    end
+
+    def funding_rounds
+        FundingRound.all.select { |funding_round| funding_round.startup == self }
+    end
+
+    def investors
+        funding_rounds.map { |funding_round| funding_round.venture_capitalist }.uniq
+    end
+
+    def big_investors
+        investors.select { |investor| VentureCapitalist.tres_commas_club.include?(investor) }
+    end
+
+    def num_funding_rounds
+        funding_rounds.count
+    end
+
+    def total_funds
+        funding_rounds.reduce(0) { |total_funds, funding_round| total_funds + funding_round.investment }
+    end
+
+    def self.find_by_founder(name)
+        self.all.find { |startup| startup.founder == name}
+    end
+
+    def self.domains
+        self.all.map { |startup| startup.domain }.uniq
+    end
+
+    def self.all
+        @@all
+    end
 end
